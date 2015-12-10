945053102f1f2245bb776b3a28df0c1c97dc1a51
Jannick Hemelhof <jhemelho@vub.ac.be>
2015-12-10 09:40:36 +0100

    basic imgur service implemented

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,4 +1,16 @@
-ufoApp.controller('homeCtrl', ['$scope', 'auth', function($scope, auth){
+ufoApp.controller('homeCtrl', ['$scope', 'auth', 'imgur', function($scope, auth, imgur){
     $scope.title = "home Page!";
     $scope.isLoggedIn = auth.isLoggedIn;
+
+    var imgurStuff = new imgur('09da5770eeb1e29');
+
+    imgurStuff.getGalleryTag("ufo").then(function(result) {
+        console.log(result);
+        console.log(result.data);
+        console.log(result.data.items);
+        console.log(result.data.items[0]);
+        $scope.getResult = result.data.items[0];
+    }, function(error) {
+        console.error(error);
+    });
 }]);
---
+++ public/js/services/imgur.js
@@ -0,0 +1,42 @@
+ufoApp.factory('imgur', ["$q", "$http", function($q, $http) {
+
+    var imgurObj = function(client_id) {
+        this.client_id = client_id;
+        this.api = "https://api.imgur.com/3";
+    };
+
+    imgurObj.prototype = {
+        getGalleryTag: function(t_name, sort, page, window) {
+            var deferred = $q.defer();
+            var endPoint = this.api + "/gallery/t";
+            endPoint += "/" + t_name;
+            // Pass parameters if they were provided
+            if (sort !== undefined) {
+                endPoint += "/" + sort;
+            }
+            if (window !== undefined) {
+                endPoint += "/" + window;
+            }
+            if (page !== undefined) {
+                endPoint += "/" + page;
+            }
+            $http({
+                method: "GET",
+                url: endPoint,
+                headers: {
+                    "Authorization": "Client-ID " + this.client_id
+                }
+            })
+                .success(function(result) {
+                    deferred.resolve(result);
+                })
+                .error(function(error) {
+                    deferred.reject(error);
+                });
+            return deferred.promise;
+        }
+    };
+
+    return imgurObj;
+
+}]);
