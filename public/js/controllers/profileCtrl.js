0578b86fe0dfd4511c824d354e92c65159f5c76f
Jannick Hemelhof <jhemelho@vub.ac.be>
2015-12-14 08:29:20 +0100

    Added more notifications

--- public/js/controllers/profileCtrl.js
+++ public/js/controllers/profileCtrl.js
@@ -4,6 +4,31 @@ ufoApp.controller('profileCtrl', ['$scope','auth', 'sightings',
 		$scope.sighting = {};
 		
 		var username = auth.currentUser();
+
+		var Notification = window.Notification || window.mozNotification || window.webkitNotification;
+
+		// Need to ask the user for permission to show notifications
+		Notification.requestPermission(function (permission) {
+			// console.log(permission);
+		});
+
+		// Function that shows the notification
+		var showNotification = function(message) {
+			var instance = new Notification(message);
+
+			instance.onclick = function () {
+				// Something to do
+			};
+			instance.onerror = function () {
+				// Something to do
+			};
+			instance.onshow = function () {
+				// Something to do
+			};
+			instance.onclose = function () {
+				// Something to do
+			};
+		};
 		
 	    var setSightings = function(username){
 		    sightings.getUserSightings(username).
@@ -14,8 +39,8 @@ ufoApp.controller('profileCtrl', ['$scope','auth', 'sightings',
 
 	    $scope.deleteSighting = function(sighting){
 		    sightings.deleteUserSighting(sighting).
-		    	success(function(){
-	    			//alert("Sighting deleted");
+		    	success(function() {
+					showNotification("Sighting was successfully deleted");
 	    	});
 		    setSightings(username);
 	    };
@@ -26,8 +51,8 @@ ufoApp.controller('profileCtrl', ['$scope','auth', 'sightings',
 
 	    $scope.editSighting = function(sighting){
 		    sightings.editUserSighting(sighting).
-		    	success(function(){
-	    			//alert("Sighting edited");
+		    	success(function() {
+					showNotification("Sighting was successfully edited");
 	    	});
 		    setSightings(username);
 	    };
