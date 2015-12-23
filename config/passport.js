3da50fde1bfd6eed5c6b3db74af4d47ed9a5d830
Jannick Hemelhof <jhemelho@vub.ac.be>
2015-12-23 09:41:19 +0100

    Added needed comments to authentication service

--- config/passport.js
+++ config/passport.js
@@ -3,20 +3,32 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
+// Authentication with passport-local (username + password)
+// Based on the example found in the support documentation
+// --> http://passportjs.org/docs/username-password
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({username: username}, 
-      function (err, user) {
-        if (err) {return done(err);}
-        if (!user) {
-        return done(null, false, 
-          {message: 'Incorrect username.'});
-        }
-        if (user.password != password) {
-        return done(null, false, 
-          {message: 'Incorrect password.'});
-        }
-        return done(null, user);
-    });
+      // We're gonna look in the DB for the user
+
+      User.findOne({username: username}, function (err, user) {
+          if (err) {
+              // There was an error while searching, return it
+              return done(err);
+          }
+          if (!user) {
+              // We didn't find a user, so probably an incorrect username
+              return done(null, false, {
+                  message: 'Username not found'
+              });
+          }
+          if (user.password != password) {
+              // Password didn't match, say it
+              return done(null, false, {
+                  message: 'Password didn\'t match'
+              });
+          }
+          // There were no errors and all parameters matched. Return with the success
+          return done(null, user);
+      });
   }
 ));
